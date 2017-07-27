b95a828be70b1b400a184444e5431ec619ae0630
Andrew Tsui <andrew.tsui@ndm.ox.ac.uk>
2017-07-27 12:49:22 +0100

    Basic project structure

---
+++ src/main/java/brc/basex/BasexApplication.java
@@ -0,0 +1,13 @@
+package brc.basex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasexApplication.class, args);
+	}
+
+}
